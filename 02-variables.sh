8f297d5c27c7de19fdc8a123fdd94a21023ca2bd
kalyantalbothula <kalyan6166@gmail.com>
2025-06-26 16:56:58 +1000

    added the 2nd script

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conversation between two people
+
+echo "Ramesh:: hello suresh,good morning"
+echo "suresh:: hi Ramesh, very good morning"
+echo "Ramesh:: how are you suresh"
+echo "suresh:: I'm good, what about you"
+echo "Ramesh::  good thanks for asking"
