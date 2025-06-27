8b22b9e3a61e0f6254b1e257176548fc7d661a1f
kalyantalbothula <kalyan6166@gmail.com>
2025-06-27 14:55:29 +1000

    practicing 9th script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -24,8 +24,8 @@ yum install git -y
 
 if [ $? -ne 0 ]
 then 
-    echo "ERROR: Installing git is failed"
+    echo "ERROR: Installing Git is failed"
     exit 1
 else
-    echo "Installing git is success"
+    echo "Installing Git is success"
 fi
