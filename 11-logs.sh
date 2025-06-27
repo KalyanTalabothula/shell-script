b26a1265c155790104fa7f1a00e4dfaf672d96ee
kalyantalbothula <kalyan6166@gmail.com>
2025-06-27 17:07:03 +1000

    practicing 11th script

--- 11-logs.sh
+++ 11-logs.sh
@@ -13,10 +13,10 @@ echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){     # we implemented Validation
     if [ $1 -ne 0 ]
     then 
-        echo "ERROR: $2... $R failed $N"
+        echo -e "ERROR: $2... $R failed $N"
     exit 1
     else
-        echo "$2 ... $G success $N"
+        echo -e "$2 ... $G success $N"
      fi
 }
 
