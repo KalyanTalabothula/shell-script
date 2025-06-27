63ea0f2249ff89ab0b1932016e5e3225870009dc
kalyantalbothula <kalyan6166@gmail.com>
2025-06-27 17:05:22 +1000

    practicing 11th script

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,23 +2,28 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date -%F-%H-%M-%S)
+R="\e[31m"   # we implemented colurs
+G="\e[32m"
+N="\e[0m"
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
+LOGFILE="/tmp/$0-$TIMESTAMP.log" # we implemented logfile
 
-VALIDATE(){
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){     # we implemented Validation
     if [ $1 -ne 0 ]
     then 
-        echo "ERROR: $2... failed"
+        echo "ERROR: $2... $R failed $N"
     exit 1
     else
-        echo "$2 ... success"
+        echo "$2 ... $G success $N"
      fi
 }
 
 
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR : please run the script with the root user"
+    echo -e "$R ERROR : please run the script with the root user $N "
     exit 1 # means it is not zero, it will stop here, it won,t run whole script.
 else
     echo "you are in root user mode"
