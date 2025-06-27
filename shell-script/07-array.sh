e8fa2a91d88a017462df4df54ef79b08c3d1ab50
kalyantalbothula <kalyan6166@gmail.com>
2025-06-27 13:47:05 +1000

    practicing 7th script

---
+++ shell-script/07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS=("apple" "Banana" "Pomo" "carrot")
+
+echo "0th value : ${FRUITS[0]}"
+echo "1th value : ${FRUITS[1]}"
+echo "2th value : ${FRUITS[2]}"
+echo "3th value : ${FRUITS[3]}"
+
+echo "all values : ${FRUITS[@]}"
