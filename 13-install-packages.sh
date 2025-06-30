29ac7a0c3e7e1b0df251ad25d1eda9245e8b36d9
kalyantalbothula <kalyan6166@gmail.com>
2025-06-30 13:32:59 +1000

    Add files via upload

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0]
+    then 
+        echo -e "$R ERROR:: $2 ... $R  Failed $N"
+        exit 1
+    else 
+        echo -e " $2 ... $G Success $N"
+    fi
+}
+
+
 
 if [ $ID -ne 0 ]
 then 
